ff7f558b347b03fa5e77a0ff7e6476309dbac0a5
nkmr-jp <nkmr.git@gmail.com>
2019-07-09 20:16:37 +0900

    :art: :wrench: add .prettierrc. auto fix.

--- src/router.js
+++ src/router.js
@@ -1,23 +1,23 @@
-import Vue from "vue";
-import Router from "vue-router";
-import HomeView from "./components/views/HomeView";
-import AboutView from "./components/views/AboutView";
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomeView from './components/views/HomeView'
+import AboutView from './components/views/AboutView'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/about',
+      name: 'about',
       component: AboutView
     }
   ]
-});
+})
